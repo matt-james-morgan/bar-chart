3b26d42e46115e4b6f4f08af5e1a29cabba1be62
matt-james-morgan~ <matt.james.morgan97@gmail.com>
2023-09-08 15:18:43 -0400

    function to collect data from user

--- javascript/data.js
+++ javascript/data.js
@@ -1,3 +1,25 @@
 const userData = {
-    
+    options:{
+        spacing: 0,
+        axis: 0,
+        barColor: 0,
+        labelColor: 0,
+        fontSize: '',
+        fontColor: ''
+    },
+    data: [],
+    element: 0
 }
+
+
+const assignData = (data, options, element) =>{
+    userData.data = data;
+    Object.keys(userData.options).forEach(function(key){
+        userData.options[key] = options[key];
+    });
+}
+
+$("#enterData").click(function(data, options, element){
+    assignData(data,options, element);
+})
+
