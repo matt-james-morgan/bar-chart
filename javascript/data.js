b7a09e6cec58745baf3a6cba311905e421f524a0
matt-james-morgan~ <matt.james.morgan97@gmail.com>
2023-09-21 20:47:37 -0400

    added back in assignData function and got bar color picker working

--- javascript/data.js
+++ javascript/data.js
@@ -4,26 +4,51 @@ let graphData = {
     barLabelName: []
 }
 
+const checkDataForString = (data) => {
+    return !data.some(isNaN);
+}
+
+const assignData = () => {
+    let data = $("#data").val().replace(/\s/g, '').split(',').map((x) => {
+        return parseInt(x);
+    });
+    try {
+        if (!checkDataForString(data)) {
+            throw new Error('Non-numeric values found in data');
+        }
+    } catch (error) {
+        throw new Error('Error');
+    }
+    graphData.data = data;
+}
+
+//on click event to assign values to Graph Data object 
 $("#chart-name-submit").click((e)=>{
     graphData.title = $("#graph-title").val();
     graphData.titleColor = $("#title-color").val();
     graphData.titleSize = $("#graph-title-size").val();
-    if(!graphData.title){
+    if(!graphData.title){//catches error if user hasn't filled out form
         e.preventDefault();
         alert('Please fill out form');
     }else{
-        e.preventDefault();
+        e.preventDefault(); //displays next section
         $("#chart-name-div").hide();
         $("#enter-data").show();
     } 
 })
 
-const assignBarColor = () =>{
-    const ul = document.createElement('ul');
+const generateBarColorInput = () =>{
+    const div = document.createElement('div');
 
-    graphData.data.map((x, i)=>{
-        
-    })
+    graphData.data.map((x, i)=>{//mapping each piece of data to generate new color option for each bar
+        const label = document.createElement('label');
+        const input = document.createElement('input');
+        label.innerHTML = 'Bar ' + (i+1);
+        input.type = 'color';
+        div.append(label, input);
+    });
+
+    $("#assign-bar-color").append(div);
 }
 
 
@@ -35,6 +60,8 @@ $("#data-submit").click((e)=>{
     }else{
         e.preventDefault();
         $("#enter-data").hide();
+        assignData();
+        generateBarColorInput();
         $("#assign-bar-color").show();
     } 
 })
